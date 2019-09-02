77b768d2349e5aa3ae628fffcdded736d15ba94f
eli <pc_ghost@hotmail.com>
2019-09-02 22:17:49 +0800

    first commit

---
+++ property/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Employee(models.Model):
+    pass
+
+
+class Equipment(models.Model):
+    pass
+
+
+class Company(models.Model):
+    pass
+
+
+class Station(models.Model):
+    pass
