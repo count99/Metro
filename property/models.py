56d4dcd6db094735fea830a5aae029c5263638bc
eli <pc_ghost@hotmail.com>
2019-09-04 09:53:09 +0800

    change apps verbose_name and set model note to allow blank

--- property/models.py
+++ property/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Apart(models.Model):
     apart_name = models.CharField(verbose_name="股名", max_length=20)
-    note = models.TextField(verbose_name="備註")
+    note = models.TextField(verbose_name="備註", blank=True, null=True)
 
     def __str__(self):
         return self.apart_name
@@ -18,9 +18,9 @@ class Apart(models.Model):
 class Employee(models.Model):
     employee_number = models.CharField(verbose_name="員工編號", max_length=20)
     employee_name = models.CharField(verbose_name="姓名", max_length=20)
-    apart = models.ForeignKey(Apart, on_delete=models.CASCADE)
+    apart = models.ForeignKey(Apart, verbose_name="所屬部門", on_delete=models.CASCADE)
     password = models.CharField(verbose_name="密碼", max_length=50)
-    note = models.TextField(verbose_name="備註")
+    note = models.TextField(verbose_name="備註", blank=True, null=True)
 
     def __str__(self):
         return self.employee_name
@@ -32,12 +32,12 @@ class Employee(models.Model):
 
 class Station(models.Model):
     station_name = models.CharField(verbose_name="站名", max_length=20)
-    apart = models.ForeignKey(Apart, on_delete=models.CASCADE)
+    apart = models.ForeignKey(Apart, verbose_name="所屬部門", on_delete=models.CASCADE)
     station_number = models.CharField(verbose_name="車站編號", max_length=20)
     station_ip_1 = models.CharField(verbose_name="通訊所屬ip網段1", max_length=20)
     station_ip_2 = models.CharField(verbose_name="通訊所屬ip網段2", max_length=20)
     station_ip_3 = models.CharField(verbose_name="資訊所屬ip網段1", max_length=20)
-    note = models.TextField(verbose_name="備註")
+    note = models.TextField(verbose_name="備註", blank=True, null=True)
 
     def __str__(self):
         return self.station_name
@@ -53,10 +53,10 @@ class Product(models.Model):
     product_type = models.CharField(verbose_name="類型", max_length=20)
     product_account = models.CharField(verbose_name="產品預設帳號", max_length=20, blank=True, null=True)
     product_password = models.CharField(verbose_name="產品預設密碼", max_length=20, blank=True, null=True)
-    note = models.TextField(verbose_name="備註")
+    note = models.TextField(verbose_name="備註", blank=True, null=True)
 
     def __str__(self):
-        return self.product_name
+        return self.company_name
 
     class Meta:
         verbose_name = '設備型號管理'
@@ -65,12 +65,12 @@ class Product(models.Model):
 
 class Equipment(models.Model):
     equipment_name = models.CharField(verbose_name="設備名稱", max_length=50)
-    station = models.ForeignKey(Station, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    station = models.ForeignKey(Station, verbose_name="所屬車站", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name="設備型號", on_delete=models.CASCADE)
     ip = models.CharField(verbose_name="設備ip", max_length=20)
     status = models.CharField(verbose_name="設備狀態", max_length=20)
     setup_time = models.DateField(verbose_name="安裝日期", auto_now_add=True)
-    note = models.TextField(verbose_name="備註")
+    note = models.TextField(verbose_name="備註", blank=True, null=True)
 
     def __str__(self):
         return self.equipment_name
